2c589e17071beb8eddb26e3742b5309647ae8d42
34308 <34308@student.pwsztar.edu.pl>
2023-02-27 20:35:50 +0100

    develop 1
    develop 1 send chodzenie obrót postaci wskaznik myszy

---
+++ Assets/Scripts/PointerHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerHandler : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       Ray ray=mainCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out RaycastHit raycastHit))
+        {
+            transform.position = raycastHit.point;
+        }
+    }
+}
