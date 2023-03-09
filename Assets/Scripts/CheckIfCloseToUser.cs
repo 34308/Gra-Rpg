9ee08321efac782444036852c1afb29debefc651
Broniu <broniekf@gmail.com>
2023-03-09 17:01:49 +0100

    add slime attack

---
+++ Assets/Scripts/CheckIfCloseToUser.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfCloseToUser : MonoBehaviour
+{
+    Animator _animator;
+    GameObject _player;
+
+    private static readonly int IsCloseToPlayer = Animator.StringToHash("isCloseToPlayer");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool(IsCloseToPlayer, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool(IsCloseToPlayer, false);
+        }
+    }
+}
